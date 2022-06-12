dc77ae1c5dec36290dd22310d6ac702b0c79a359
Danish Shafi <danish.shafi@live.co.uk>
2022-06-12 17:29:53 +0100

    App finished

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const app = new function() {
         data += '<td><button onclick="app.Edit(' + i + ')"  class="btn btn-warning">Edit</button></td>';
         data += '<td><button onclick="app.Delete(' + i + ')"  class="btn btn-danger">Delete</button></td>';
         data += '</tr>';
+
+        console.log(data)
       }
     }
     this.Count(this.tasks.length);
@@ -30,109 +32,47 @@ const app = new function() {
   };
 
   this.Edit = function(item) {
+    let el = document.getElementById('edit-todo');
 
+    el.value = this.tasks[item];
+
+    document.getElementById('edit-box').style.display = 'block';
+    self = this;
+
+    document.getElementById('save-edit').onsubmit = function() {
+      let task = el.value;
+
+      if (task) {
+        self.tasks.splice(item, 1, task.trim());
+        self.fetchAll();
+        closeInput();
+      }
+    }
   }
 
   this.Delete = function(item) {
-
+    this.tasks.splice(item, 1);
+    this.fetchAll();
   }
 
   this.Count = function(data) {
+    let el = document.getElementById('counter');
+    let name = 'Tasks';
 
-  }
-}
+    if (data) {
+      if (data == 1) {
+        name = 'Task';
+      }
+      el.innerHTML = `${data} ${name}`;
+    }
+    else {
+      el.innerHTML = `No ${name}`;
+    }
+  };
+};
 
 app.fetchAll();
 
 const closeInput = () => {
   document.getElementById('edit-box').style.display = 'none';
 };
-
-
-// var app = new function() {
-//   this.el = document.getElementById('tasks');
-
-//   this.tasks = [];
-
-
-
-//   this.FetchAll = function() {
-//     var data = '';
-
-//     if (this.tasks.length > 0) {
-//       for (i = 0; i < this.tasks.length; i++) {
-//         data += '<tr>';
-//         data += '<td>'+(i+1)+". " + this.tasks[i] + '</td>';
-//         data += '<td><button onclick="app.Edit(' + i + ')"  class="btn btn-warning">Edit</button></td>';
-//         data += '<td><button onclick="app.Delete(' + i + ')"  class="btn btn-danger">Delete</button></td>';
-//         data += '</tr>';
-//       }
-//     }
-
-//     this.Count(this.tasks.length);
-//     return this.el.innerHTML = data;
-//   };
-
-//   this.Add = function () {
-//     el = document.getElementById('add-todo');
-//     // Get the value
-//     var task = el.value;
-
-//     if (task) {
-//       // Add the new value
-//       this.tasks.push(task.trim());
-//       // Reset input value
-//       el.value = '';
-//       // Dislay the new list
-//       this.FetchAll();
-//     }
-//   };
-
-//   this.Edit = function (item) {
-//     var el = document.getElementById('edit-todo');
-//     // Display value in the field
-//     el.value = this.tasks[item];
-//     // Display fields
-//     document.getElementById('edit-box').style.display = 'block';
-//     self = this;
-
-//     document.getElementById('save-edit').onsubmit = function() {
-//       // Get value
-//       var task = el.value;
-
-//       if (task) {
-//         // Edit value
-//         self.tasks.splice(item, 1, task.trim());
-//         // Display the new list
-//         self.FetchAll();
-//         // Hide fields
-//         CloseInput();
-//       }
-//     }
-//   };
-
-//   this.Delete = function (item) {
-//     // Delete the current row
-//     this.tasks.splice(item, 1);
-//     // Display the new list
-//     this.FetchAll();
-//   };
-
-//   this.Count = function(data) {
-//     var el   = document.getElementById('counter');
-//     var name = 'Tasks';
-
-//     if (data) {
-//         if(data ==1){
-//             name = 'Task'
-//         }
-//       el.innerHTML = data + ' ' + name ;
-//     }
-//     else {
-//       el.innerHTML = 'No ' + name;
-//     }
-//   };
-
-// }
-
-// app.FetchAll();
